44b2244f588fb7fa477da662a357ae0221ce6947
Nicolas <nicolasbarrapelecano@gmail.com>
2022-07-31 14:07:07 -0300

    agregados ejercicios tkinter

--- grafica/ejercicio_1.py
+++ grafica/ejercicio_1.py
@@ -3,4 +3,43 @@
 #Al principio no tiene que haber una opción seleccionada.
 
 import tkinter as tk
+from tkinter import ttk
+#funciones
+
+def mostrar_seleccion():
+    #genero la etiqueta
+    global label_show #para que la etiqueta este disponible fuera del scope de la funcion!
+    label_show = ttk.Label(window, text=f'el valor elegido es: {selected.get()}')
+    label_show.grid(row=1, column=2, sticky=tk.W)
+    
+
+
+#genero la ventana
+window = tk.Tk()
+window.title("Ejercicio 1")
+window.columnconfigure(0, weight=1)
+window.columnconfigure(1, weight=3)
+
+#genero la etiqueta
+label = ttk.Label(window, text="Ejercicio 1")
+label.grid(row=0, column=0, sticky=tk.W)
+
+#genero los radiobutton
+selected = tk.StringVar()
+rad_1 = ttk.Radiobutton(window, text="uno", variable=selected, value=1, command=mostrar_seleccion)
+rad_1.grid(row=1, column=0, sticky=tk.W)
+
+rad_2 = ttk.Radiobutton(window, text="dos", variable=selected, value=2, command=mostrar_seleccion)
+rad_2.grid(row=2, column=0, sticky=tk.W)
+
+rad_3 = ttk.Radiobutton(window, text="tres", variable=selected, value=3, command=mostrar_seleccion)
+rad_3.grid(row=3, column=0, sticky=tk.W)
+
+#genero el boton de reinicio
+reset_btn = ttk.Button(window, text="Reset", command=lambda: (selected.set(''), label_show.grid_remove()))
+reset_btn.grid(row=1, column=3, padx=5, pady=5,sticky=tk.W)
+
+
+window.mainloop()
+
 
--- grafica/ejercicio_2.py
+++ grafica/ejercicio_2.py
@@ -1,2 +1,24 @@
 #En este segundo ejercicio, tendréis que crear una interfaz sencilla la cual debe de contener una lista de elementos seleccionables, también debe de tener un label con el texto que queráis.
 
+import tkinter as tk
+from tkinter import Listbox, StringVar, ttk
+#genero la ventana
+window = tk.Tk()
+window.title("Ejercicio 1")
+window.columnconfigure(0, weight=1)
+window.columnconfigure(0, weight=3)
+
+#genero el label
+label = ttk.Label(window, text="Ejercicio 1")
+label.grid(row=0, column=0, sticky=tk.W)
+
+#genero la lista con sus items
+lista = ['uno', 'dos', 'tres', 'cuatro', 'cinco']
+lista_items = tk.StringVar(value=lista)
+listbox = tk.Listbox(window,height = 6, listvariable=lista_items)
+listbox.grid(row=1, column=0, sticky=tk.W)
+
+exit_btn = ttk.Button(window, text="Salir", command=lambda: window.quit())
+exit_btn.grid(row=1, column=3, padx=5, pady=5,sticky=tk.W)
+
+window.mainloop()

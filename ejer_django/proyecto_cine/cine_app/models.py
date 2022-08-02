ba0528dd5d1e08ee9be63148ba9b42533d9328f9
Nicolas <nicolasbarrapelecano@gmail.com>
2022-08-02 18:36:04 -0300

    agregados modelos al admin

--- ejer_django/proyecto_cine/cine_app/models.py
+++ ejer_django/proyecto_cine/cine_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+#En este ejercicio tendrás que crear una aplicación en Django que almacene datos de directores de cine y luego se puedan ver sus películas, así como una descripción de las mismas.
+#Tienes que personalizar el admin de la aplicación y a su vez crear las vistas de cada una de las partes de la aplicación.
 
-# Create your models here.
+class Directores(models.Model):
+    name = models.CharField(max_length=60, help_text='Nombre del director')
+    
+    def __str__(self) -> str:
+        return self.name
+    
+class Movies(models.Model):
+    name = models.CharField(max_length=60, help_text='Nombre de la película')
+    director = models.ForeignKey(Directores, on_delete=models.SET_NULL, null=True)
+    description = models.TextField(max_length=200, help_text='Descripción de la película')
+    year = models.IntegerField(help_text='Año de la película')
+    image = models.ImageField(upload_to='movies', help_text='Imagen de la película')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self) -> str:
+        return self.name
--- ejer_django/proyecto_cine/cine_app/views.py
+++ ejer_django/proyecto_cine/cine_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Directores, Movies
+
+
+def index(request):
+    num_movies = Movies.objects.all().count()
+    cant_directores = Directores.objects.all().count()
+    
+    return render(request, 'cine_app/index.html', context={
+        'num_movies': num_movies, 
+        'cant_directores': cant_directores
+        })
+    

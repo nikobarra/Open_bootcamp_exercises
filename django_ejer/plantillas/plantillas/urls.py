fa9a2e38a0233057f1e8a4733fb66814e358dd7c
Nicolas Barra <nicolasbarrapelecano@gmail.com>
2022-08-12 20:20:03 -0300

    agregado context

--- django_ejer/plantillas/plantillas/urls.py
+++ django_ejer/plantillas/plantillas/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('simple/', views.simple, name= 'simple'),
+    path('dinamico/<str:name>',views.dinamico , name ='dinamico'),
 ]
--- django_ejer/plantillas/plantillas/views.py
+++ django_ejer/plantillas/plantillas/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def simple(request):
     return render(request, 'simple.html')
+
+def dinamico(request, name):
+    context = {'name':name}
+    return render(request, 'dinamico.html', context)

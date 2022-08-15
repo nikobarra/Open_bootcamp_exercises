f8a6d73649c34e0227c9fda6d9d4649aa4313514
Nicolas Barra <nicolasbarrapelecano@gmail.com>
2022-08-15 17:15:34 -0300

    linkeado de app a proyecto base

--- django_ejer/modularizando/comentarios/models.py
+++ django_ejer/modularizando/comentarios/models.py
@@ -5,9 +5,10 @@ class Comments(models.Model):
     name = models.CharField(max_length=100, null=False)
     score = models.IntegerField(default=3)
     comment = models.TextField(max_length=1000, null=True)
-    data_comment = models.DateTimeField(auto_now_add=True)
+    date_comment = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
     
+
 

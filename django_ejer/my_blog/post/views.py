e5457a9065144d0b01d4d81cea0931bda727b846
Nicolas Barra <nicolasbarrapelecano@gmail.com>
2022-08-17 18:35:16 -0300

    busquedas

--- django_ejer/my_blog/post/views.py
+++ django_ejer/my_blog/post/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import Author, Entry
 
-# Create your views here.
+def queries(request):
+    #obtener todos los autores
+    authors = Author.objects.all()
+    
+    #obtener datos filtrados por condicion
+    filtered = Author.objects.filter(email ='tyler03@example.com')
+    
+    #obterner un unico objeto
+    
+    author = Author.objects.get(id=1)
+    
+    #busqueda con limites por ejemplo 10 primeros registros
+    limits = Author.objects.all()[:10]
+    
+    #busqueda con offset (comenzar desde un punto especifico)
+    offsets = Author.objects.all()[5:10]
+    
+    return render(request, 'post/queries.html',{'authors':authors, 'filtered':filtered, 'author':author, 'limits':limits, 'offsets':offsets})
+

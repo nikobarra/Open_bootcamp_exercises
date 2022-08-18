5fcc16542b0ee186a3b05d4820df2a6ed8e2deb1
Nicolas Barra <nicolasbarrapelecano@gmail.com>
2022-08-18 18:36:38 -0300

    mas busquedas agregadas

--- django_ejer/my_blog/post/views.py
+++ django_ejer/my_blog/post/views.py
@@ -18,5 +18,27 @@ def queries(request):
     #busqueda con offset (comenzar desde/hasta un punto especifico slicing)
     offsets = Author.objects.all()[5:10]
     
-    return render(request, 'post/queries.html',{'authors':authors, 'filtered':filtered, 'author':author, 'limits':limits, 'offsets':offsets})
+    #obtener todos los elementos ordenados
+    orders = Author.objects.all().order_by('email')
+    
+    #obtener todos los elementos cuyo id sea menor o igual a 15
+    #__lte: menor o igual
+    #__gte: mayor o igual
+    #__lt: menor que
+    #__gt: mayor que
+    #__ne: diferente de
+    #__in: pertenece a una lista
+    #__contains: contiene una cadena
+    #__icontains: contiene una cadena en mayusculas o minusculas
+    #__startswith: empieza con una cadena
+    #__istartswith: empieza con una cadena en mayusculas o minusculas
+    #__endswith: termina con una cadena
+    #__iendswith: termina con una cadena en mayusculas o minusculas
+    #__isnull: es nulo
+    
+    filtered_2 = Author.objects.filter(id__lte=15)
+    filtered_3=Author.objects.filter(name__contains = 'win')
+    
+    
+    return render(request, 'post/queries.html',{'authors':authors, 'filtered':filtered, 'author':author, 'limits':limits, 'offsets':offsets, 'orders':orders, 'filtered_2':filtered_2, 'filtered_3':filtered_3})
 

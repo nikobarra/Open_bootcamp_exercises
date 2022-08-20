4a783f4644a6ce9f186b4b5cb9a3dbcb51b88368
Nicolas Barra <nicolasbarrapelecano@gmail.com>
2022-08-20 18:01:33 -0300

    modelos con relaciones

--- django_ejer/practica_modelos/company/models.py
+++ django_ejer/practica_modelos/company/models.py
@@ -1,22 +1,5 @@
 from django.db import models
 
-class Employee(models.Model):
-    id_number = models.CharField(max_length=20, blank=False, null=False, unique=True)
-    name = models.CharField(max_length=50, blank=False, null=False)
-    last_name= models.CharField(max_length=50, blank=False, null=False)
-    email = models.EmailField(blank=False, null=False)
-    address = models.CharField(max_length=50, blank=False, null=False)
-    
-    def __str__(self):
-        return self.name
-    
-class Job(models.Model):
-    title = models.CharField(max_length=20, blank=False, null=False)
-    description = models.TextField(null=True)
-    
-    def __str__(self):
-        return self.title
-    
 class Salary(models.Model):
     amount = models.IntegerField (blank=False, null=False)
     extra_dec=models.BooleanField(default=False)
@@ -25,26 +8,52 @@ class Salary(models.Model):
     def __str__(self):
         return self.amount
 
-class Place(models.Model):
-    name = models.CharField(max_length=50, blank=False, null=False)
-    address = models.CharField(max_length=50, blank=False, null=False)
-    zip_code = models.CharField(max_length=10, blank=False, null=False)
-    
-    def __str__(self):
-        return self.name
-    
 
-class Location (models.Model):
-    name = models.CharField(max_length=20, blank=False, null=False)
-    
+class Job(models.Model):
+    title = models.CharField(max_length=20, blank=False, null=False)
+    description = models.TextField(null=True)
+    salary = models.ForeignKey(Salary, on_delete=models.CASCADE)
     
     def __str__(self):
-        return self.name
-    
+        return self.title
+
+
 class Country(models.Model):
     name = models.CharField(max_length=30, blank=False, null=False)
     country_code = models.CharField(max_length=5, blank=False, null=False)
 
+
     def __str__(self):
         return self.name
     
+    
+class Location (models.Model):
+    name = models.CharField(max_length=20, blank=False, null=False)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+    
+    
+class Place(models.Model):
+    name = models.CharField(max_length=50, blank=False, null=False)
+    address = models.CharField(max_length=50, blank=False, null=False)
+    zip_code = models.CharField(max_length=10, blank=False, null=False)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+
+
+class Employee(models.Model):
+    id_number = models.CharField(max_length=20, blank=False, null=False, unique=True)
+    name = models.CharField(max_length=50, blank=False, null=False)
+    last_name= models.CharField(max_length=50, blank=False, null=False)
+    email = models.EmailField(blank=False, null=False)
+    address = models.CharField(max_length=50, blank=False, null=False)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+    
+    
+    def __str__(self):
+        return self.name

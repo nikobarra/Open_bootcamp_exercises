09cab4c094b8ec301e1977937425635cd59d49ac
Nicolas Barra <nicolasbarrapelecano@gmail.com>
2022-08-23 18:17:28 -0300

    uso admin django

--- django_ejer/ecommerce/stock/admin.py
+++ django_ejer/ecommerce/stock/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'short_description', 'description', 'stock', 'price')
+    list_filter = ('name', 'short_description', 'description', 'stock', 'price')
+    search_fields = ('name', 'short_description', 'description', 'stock', 'price')
+    ordering = ('name', 'short_description', 'description', 'stock', 'price')
+    filter_horizontal = ()
+    fieldsets = ()
+
+admin.site.register(Product,ProductAdmin )
+
+
+
+
+
--- django_ejer/ecommerce/stock/models.py
+++ django_ejer/ecommerce/stock/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import date
 
 class Product(models.Model):
     name = models.CharField(max_length=100, blank=False, null=False)
@@ -6,6 +7,7 @@ class Product(models.Model):
     description = models.TextField(blank=False, null=False)
     stock = models.IntegerField(default=20, blank=False, null=False)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField(default=date.today)
 
     def __str__(self):
         return self.name
